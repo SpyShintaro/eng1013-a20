6b53ddadfefa6d4143a75eb9b3cb8e10302e4fa8
Abbey <hubbardabbey@gmail.com>
2025-05-22 18:01:19 +1000

    stupid buzzer

--- modules/wl.py
+++ modules/wl.py
@@ -3,8 +3,29 @@ import time
 
 board = pymata4.Pymata4()
 
-board.set_pin_mode_digital_output(8)
+timer1 = 13
+timer2 = 12
 
-board.digital_write(8, 1)
+board.set_pin_mode_digital_output(timer1)
 
+board.set_pin_mode_digital_output(timer2)
+
+
+"""try:
+        board.digital_write(13, 1)
+        board.digital_write(12, 0)
+        time.sleep(5)
+        print("changing")
+        board.digital_write(13, 0)
+        board.digital_write(12, 1)
+        time.sleep(5)
+    except KeyboardInterrupt:
+        print("ending")
+        board.digital_write(timer1, 0)
+        board.digital_write(timer2, 0)
+        board.shutdown()"""
+ 
+board.digital_write(12, 1)
+time.sleep(4)
+board.digital_write(12, 1)
 board.shutdown()
